f9e1351c86cb2c825a967a1217b41b4c4a6afa9f
YashPimpalkar <vu1s2223002@pvppcoe.ac.in>
2024-06-03 17:36:37 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,77 @@
+function openFullscreen(src) {
+    // Function to open images and videos in fullscreen
+    const elem = document.documentElement;
+    if (elem.requestFullscreen) {
+        elem.requestFullscreen();
+    } else if (elem.mozRequestFullScreen) {
+        elem.mozRequestFullScreen();
+    } else if (elem.webkitRequestFullscreen) {
+        elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) {
+        elem.msRequestFullscreen();
+    }
+}
+
+window.onload = function() {
+    const galleryContainer = document.querySelector('.gallery');
+
+    // Generate image elements from 1 to 82
+    for (let i = 2; i <= 58; i++) {
+        const img = document.createElement('img');
+        img.src = `Pawna-Lake (${i}).jpeg`;
+        img.alt = `Image ${i}`;
+        img.className = 'gallery-item';
+        img.setAttribute('onclick', `openFullscreen('${img.src}')`);
+        img.loading ="lazy";
+        // Add rotation class to a specific image
+        // if (i === 58) {
+        //     img.classList.add('rotate-90'); // Example: rotate the 2nd image upside down
+        // }
+
+        galleryContainer.appendChild(img);
+
+    }
+
+
+    for (let i = 1; i <= 6; i++) {
+        const video = document.createElement('video');
+        video.src = `Pawna-Lake (${i}).mp4`; // Adjust video source accordingly
+        video.type = 'video/mp4';
+        video.className = 'gallery-item';
+        video.setAttribute('controls', '');
+        video.setAttribute('onclick', `openFullscreen('${video.src}')`);
+        
+
+        // Add rotation class to a specific video
+        // if (i === 3) {
+        //     video.classList.add('rotate-90'); // Example: rotate the 3rd video by 90 degrees
+        // }
+
+        galleryContainer.appendChild(video);
+    }
+};
+
+document.addEventListener("DOMContentLoaded", function() {
+    var lazyVideos = [].slice.call(document.querySelectorAll(".lazy-video"));
+
+    if ("IntersectionObserver" in window) {
+        var lazyVideoObserver = new IntersectionObserver(function(entries, observer) {
+            entries.forEach(function(entry) {
+                if (entry.isIntersecting) {
+                    var video = entry.target;
+                    video.src = video.dataset.src;
+                    lazyVideoObserver.unobserve(video);
+                }
+            });
+        });
+
+        lazyVideos.forEach(function(video) {
+            lazyVideoObserver.observe(video);
+        });
+    } else {
+        // For browsers that don't support IntersectionObserver, load all videos immediately
+        lazyVideos.forEach(function(video) {
+            video.src = video.dataset.src;
+        });
+    }
+});
